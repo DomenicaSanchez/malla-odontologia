8c5c48fecca933e19da0b560568615d0632b9445
Rociocerpa <rocio.cerpa@ug.uchile.cl>
2025-07-10 00:07:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Mapeo de prerrequisitos: cada ramo apunta a los ID que debe tener aprobados
+// Mapeo de prerrequisitos
 const prerequisitos = {
   'integracion1': ['fundamentos1'],
   'bioquimica': ['quimica', 'biologia'],
@@ -21,71 +21,5 @@ const prerequisitos = {
   'neonatologia2': ['neonatologia1'],
   'obstetricia2': ['obstetricia1'],
   'gineco1': ['histologia', 'fisiologia_sis'],
-  'fisiopato': ['fisiologia_sis'],
-  'infectologia': ['agentes'],
-  'farmacologia': ['fisiologia_sis', 'bioquimica'],
-  'integracion2': ['fisiologia_sis'],
-  'clinica_neonatal1': ['neonatologia2', 'fisiopato', 'infectologia', 'farmacologia', 'integracion2'],
-  'clinica_partos1': ['obstetricia2', 'fisiopato', 'infectologia', 'farmacologia', 'integracion2'],
-  'clinica_ap1': ['obstetricia2', 'gineco1', 'fisiopato', 'infectologia', 'farmacologia', 'integracion2'],
-  'clinica_puerperio': ['obstetricia2', 'fisiopato', 'infectologia', 'farmacologia', 'integracion2'],
-  'clinica_saludcom': ['integracion2'],
-  'modulo1': [],
-  'neonatologia3': ['farmacologia', 'fisiopato', 'obstetricia2'],
-  'saludcom2': ['saludcom1'],
-  'obstetricia_pat': ['obstetricia2', 'farmacologia', 'fisiopato'],
-  'gestion1': ['investigacion1'],
-  'investigacion2': ['investigacion1'],
-  'enfermeria_mq': ['neonatologia3', 'obstetricia_pat'],
-  'reproduccion': ['fisiologia_sis'],
-  'gineco_pat': ['clinica_ap1'],
-  'gestion2': ['gestion1'],
-  'investigacion3': ['investigacion2'],
-  'cs_sociales4': ['cs_sociales3'],
-  'clinica_neonatal2': ['neonatologia3', 'enfermeria_mq'],
-  'clinica_partos2': ['obstetricia_pat', 'enfermeria_mq'],
-  'clinica_ap2': ['obstetricia_pat', 'gineco_pat', 'saludcom2'],
-  'alto_riesgo': ['obstetricia_pat', 'enfermeria_mq'],
-  'clinica_mq': ['gineco_pat', 'enfermeria_mq'],
-  'modulo2': ['modulo1'],
-  'seminario1': ['investigacion3'],
-  'internado_neonatologia': ['clinica_neonatal2'],
-  'internado_obstetricia': ['clinica_partos2', 'alto_riesgo'],
-  'internado_ap': ['clinica_ap2'],
-  'internado_gineco': ['clinica_mq'],
-  'internado_electivo': [],
-  'seminario2': ['seminario1']
-};
+  'fisiopato': ['fisiologia_s_]()
 
-// Esta función actualiza el estado de desbloqueo según los ramos aprobados
-function actualizarDesbloqueos() {
-  const aprobados = Array.from(document.getElementsByClassName('aprobado')).map(r => r.id);
-
-  for (const [destino, reqs] of Object.entries(prerequisitos)) {
-    const elem = document.getElementById(destino);
-    if (!elem) continue;
-
-    const puedeDesbloquear = reqs.every(r => aprobados.includes(r));
-    if (!elem.classList.contains('aprobado')) {
-      if (puedeDesbloquear) {
-        elem.classList.remove('bloqueado');
-      } else {
-        elem.classList.add('bloqueado');
-      }
-    }
-  }
-}
-
-// Al hacer clic en un ramo
-function aprobar(id) {
-  const ramo = document.getElementById(id);
-  if (ramo.classList.contains('bloqueado')) return;
-
-  ramo.classList.toggle('aprobado');
-  actualizarDesbloqueos(); // actualiza desbloqueos dinámicamente
-}
-
-// Al cargar la página, aplica el estado inicial (esto es clave)
-window.onload = () => {
-  actualizarDesbloqueos();
-};
